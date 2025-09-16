90f4fe130aa7bc0292a06b74e43236ca24d5cf26
Rafly Herdianto <raflyherdianto25@gmail.com>
2025-09-17 06:48:40 +0700

    test actions 1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello, Flask CI/CD (public)!"
+    return render_template("index.html", title="Flask CI/CD Demo")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
